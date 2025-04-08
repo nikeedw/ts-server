6efba40125b20035ce8c05a2d9e42a8f9041c7e8
Nektarines <grozav.nikita@gmail.com>
2025-04-08 12:07:54 +0300

    empty_project

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,11 @@
 import { Express } from 'express'
 import multer from 'multer'
-import { eliminateFirebase } from '@src/handlers/eliminateFirebase'
 
 const upload = multer()
 
-export function getRoutes(app: Express) {
+export function getRoutes(_app: Express) {
     /**
      * shared getter
      * // TODO need to split specific endpoint for this context
      */
-    app.get('/eliminate', upload.none(), (req, res) => eliminateFirebase(req, res))
 }
