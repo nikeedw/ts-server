677f8edf6fd27c9405393e09fe280557391a2392
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2025-05-05 14:32:34 +0300

    chore: Replaced frontend port to 3000

--- src/middlewares/useCors.ts
+++ src/middlewares/useCors.ts
@@ -2,17 +2,13 @@ import { Request, Response, NextFunction } from 'express'
 
 export function useCors(_request: Request, res: Response, next: NextFunction) {
     try {
-        // Allow credentials (cookies, authorization headers, etc.)
         res.header('Access-Control-Allow-Credentials', 'true')
 
-        // Allow specific methods (GET, POST, PUT, DELETE, etc.)
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 
-        // Allow specific headers (you can add more if needed)
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, esp-data')
 
-        // Allow specific origins (adjust as needed for your frontend origin)
-        res.header('Access-Control-Allow-Origin', 'http://localhost:5173') // Replace with your frontend URL
+        res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
 
         // Handle pre-flight OPTIONS requests
         if (_request.method === 'OPTIONS') {
