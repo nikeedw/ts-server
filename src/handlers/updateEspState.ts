8bfb765e3641e8f429b748038433eb1eb5a34eb9
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2025-05-19 12:53:35 +0300

    fix: Fixed update esp handler

--- src/handlers/updateEspState.ts
+++ src/handlers/updateEspState.ts
@@ -2,12 +2,25 @@ import { Request, Response } from 'express'
 
 export async function updateEspState(req: Request, res: Response) {
     try {
-        const mac = req.query.mac as string
-        if (!mac) return res.status(400).json({ message: 'MAC address is required' })
+        const { mac, led_brightness, relay_state } = req.query
+
+        if (typeof mac !== 'string') {
+            return res.status(400).json({ message: 'MAC address is required' })
+        }
+
+        if (typeof led_brightness !== 'string') {
+            return res.status(400).json({ message: 'Parameter led_brightness is required' })
+        }
+
+        if (typeof relay_state !== 'string' || !['true', 'false'].includes(relay_state)) {
+            return res.status(400).json({
+                message: 'Parameter relay_state is required and should be instance of true or false',
+            })
+        }
 
         return res.status(200).json({
-            led_brightness: 512,
-            relay_state: true,
+            led_brightness,
+            relay_state: relay_state === 'true',
         })
     } catch (e) {
         console.error(e)
--- src/routes.ts
+++ src/routes.ts
@@ -13,26 +13,27 @@ export function getRoutes(app: Express) {
     app.post('/esp', upload.none(), syncEspDataWithDB)
 
     app.get('/esp', upload.none(), updateEspState)
-
-    // GET: ESP requests current led/relay state
-    // app.get('/esp', (req: Request, res: Response) => {
-    //     const mac = req.query.mac as string
-
-    //     if (!mac) {
-    //         console.error('Missing MAC address in query')
-    //         return res.status(400).json({ error: 'MAC address is required' })
-    //     }
-
-    //     console.log(`ESP requested state for MAC: ${mac}`)
-
-    //     let state = deviceStates.get(mac)
-
-    //     if (!state) {
-    //         console.log('No state found for this MAC, creating default')
-    //         state = { led_brightness: 512, relay_state: true }
-    //         deviceStates.set(mac, state)
-    //     }
-
-    //     return res.json(state)
-    // })
 }
+
+/** @backup REMOVE after release */
+// GET: ESP requests current led/relay state
+// app.get('/esp', (req: Request, res: Response) => {
+//     const mac = req.query.mac as string
+
+//     if (!mac) {
+//         console.error('Missing MAC address in query')
+//         return res.status(400).json({ error: 'MAC address is required' })
+//     }
+
+//     console.log(`ESP requested state for MAC: ${mac}`)
+
+//     let state = deviceStates.get(mac)
+
+//     if (!state) {
+//         console.log('No state found for this MAC, creating default')
+//         state = { led_brightness: 512, relay_state: true }
+//         deviceStates.set(mac, state)
+//     }
+
+//     return res.json(state)
+// })
